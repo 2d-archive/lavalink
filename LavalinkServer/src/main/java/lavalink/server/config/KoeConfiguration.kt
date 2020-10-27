4b8eab3abd6f0fe042ba490f9b3fc7c6c419099d
LolWastedJS <ml2dofficial@gmail.com>
2020-10-27 14:13:05 -0700

    add the option for epoll, packet priority, and byte buf allocator

--- LavalinkServer/src/main/java/lavalink/server/config/KoeConfiguration.kt
+++ LavalinkServer/src/main/java/lavalink/server/config/KoeConfiguration.kt
@@ -1,5 +1,12 @@
 package lavalink.server.config
 
+import io.netty.buffer.ByteBufAllocator
+import io.netty.buffer.PooledByteBufAllocator
+import io.netty.buffer.UnpooledByteBufAllocator
+import io.netty.channel.epoll.Epoll
+import io.netty.channel.epoll.EpollDatagramChannel
+import io.netty.channel.epoll.EpollEventLoopGroup
+import io.netty.channel.epoll.EpollSocketChannel
 import moe.kyokobot.koe.KoeOptions
 import moe.kyokobot.koe.codec.udpqueue.UdpQueueFramePollerFactory
 import org.slf4j.Logger
@@ -8,32 +15,60 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class KoeConfiguration(val serverConfig: ServerConfig) {
+class KoeConfiguration(val config: KoeServerConfig) {
 
-    private val log: Logger = LoggerFactory.getLogger(KoeConfiguration::class.java)
+  private val log: Logger = LoggerFactory.getLogger(KoeConfiguration::class.java)
 
-    @Bean
-    fun koeOptions(): KoeOptions = KoeOptions.builder().apply {
-        log.info("OS: " + System.getProperty("os.name") + ", Arch: " + System.getProperty("os.arch"))
-        val os = System.getProperty("os.name")
-        val arch = System.getProperty("os.arch")
+  @Bean
+  fun koeOptions(): KoeOptions = KoeOptions.builder().apply {
+    log.info("OS: " + System.getProperty("os.name") + ", Arch: " + System.getProperty("os.arch"))
+    val os = System.getProperty("os.name")
+    val arch = System.getProperty("os.arch")
 
-        // Maybe add Windows natives back?
-        val nasSupported = os.contains("linux", ignoreCase = true)
-                && arch.equals("amd64", ignoreCase = true)
+    setHighPacketPriority(config.highPacketPriority)
 
-        if (nasSupported) {
-            log.info("Enabling JDA-NAS")
-            var bufferSize = serverConfig.bufferDurationMs ?: UdpQueueFramePollerFactory.DEFAULT_BUFFER_DURATION
-            if (bufferSize <= 0) {
-                log.warn("Buffer size of {}ms is illegal. Defaulting to {}",
-                        bufferSize, UdpQueueFramePollerFactory.DEFAULT_BUFFER_DURATION)
-                bufferSize = UdpQueueFramePollerFactory.DEFAULT_BUFFER_DURATION
-            }
-            setFramePollerFactory(UdpQueueFramePollerFactory(bufferSize, Runtime.getRuntime().availableProcessors()))
-        } else {
-            log.warn("This system and architecture appears to not support native audio sending! "
-                    + "GC pauses may cause your bot to stutter during playback.")
-        }
-    }.create()
+    /* JDA-NAS */
+    // Maybe add Windows natives back?
+    val nasSupported = os.contains("linux", ignoreCase = true)
+      && arch.equals("amd64", ignoreCase = true)
+
+    if (nasSupported) {
+      log.info("Enabling JDA-NAS")
+
+      var bufferSize = config.bufferDurationMs ?: UdpQueueFramePollerFactory.DEFAULT_BUFFER_DURATION
+      if (bufferSize <= 0) {
+        log.warn("Buffer size of ${bufferSize}ms is illegal. Defaulting to ${UdpQueueFramePollerFactory.DEFAULT_BUFFER_DURATION}")
+        bufferSize = UdpQueueFramePollerFactory.DEFAULT_BUFFER_DURATION
+      }
+
+      setFramePollerFactory(UdpQueueFramePollerFactory(bufferSize, Runtime.getRuntime().availableProcessors()))
+    } else {
+      log.warn("This system and architecture appears to not support native audio sending! "
+        + "GC pauses may cause your bot to stutter during playback.")
+    }
+
+    /* Epoll Transport */
+    if (config.useEpoll && Epoll.isAvailable()) {
+      log.info("Using Epoll Transport.")
+      setEventLoopGroup(EpollEventLoopGroup())
+      setDatagramChannelClass(EpollDatagramChannel::class.java)
+      setSocketChannelClass(EpollSocketChannel::class.java)
+    }
+
+    /* Byte Buf Allocator */
+    var custom = true
+    when (config.byteBufAllocator) {
+      "netty-default" -> setByteBufAllocator(ByteBufAllocator.DEFAULT)
+      "default", "pooled" -> setByteBufAllocator(PooledByteBufAllocator.DEFAULT)
+      "unpooled" -> setByteBufAllocator(UnpooledByteBufAllocator.DEFAULT)
+      else -> {
+        log.warn("Invalid byte buf allocator \"${config.byteBufAllocator}\", defaulting to the 'pooled' byte buf allocator.")
+        custom = false
+      }
+    }
+
+    if (custom) {
+      log.info("Using the '${config.byteBufAllocator}' byte buf allocator")
+    }
+  }.create()
 }
---
+++ LavalinkServer/src/main/java/lavalink/server/config/KoeServerConfig.kt
@@ -0,0 +1,13 @@
+package lavalink.server.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@ConfigurationProperties(prefix = "lavalink.server.koe")
+@Component
+class KoeServerConfig {
+  var useEpoll: Boolean = true
+  var highPacketPriority: Boolean = true
+  var bufferDurationMs: Int? = null
+  var byteBufAllocator: String = "pooled"
+}
