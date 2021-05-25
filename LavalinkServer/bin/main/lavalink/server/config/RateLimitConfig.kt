bcc1dcd0f6a8b32539c9749432957ff5b87f2fa5
2D <gino@dimensional.fun>
2021-05-25 08:57:21 -0700

    ok

--- LavalinkServer/bin/main/lavalink/server/config/RateLimitConfig.kt
+++
@@ -1,9 +0,0 @@
-package lavalink.server.config
-
-data class RateLimitConfig(
-        var ipBlocks: List<String> = emptyList(),
-        var excludedIps: List<String> = emptyList(),
-        var strategy: String = "RotateOnBan",
-        var retryLimit: Int = -1,
-        var searchTriggersFail: Boolean = true
-)
