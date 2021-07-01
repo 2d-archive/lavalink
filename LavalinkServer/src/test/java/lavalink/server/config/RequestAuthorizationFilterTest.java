a6ca4acd785530b58c95fe810a066b07898f83b2
Restyled.io <commits@restyled.io>
2021-07-01 18:05:38 +0000

    Restyled by clang-format

--- LavalinkServer/src/test/java/lavalink/server/config/RequestAuthorizationFilterTest.java
+++ LavalinkServer/src/test/java/lavalink/server/config/RequestAuthorizationFilterTest.java
@@ -2,27 +2,30 @@
  *  Copyright (c) 2021 Freya Arbjerg and contributors
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
+ *  of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
  *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
+ *  The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  *
  */
 
 package lavalink.server.config;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import lavalink.server.info.AppInfo;
 import org.junit.jupiter.api.Test;
@@ -32,10 +35,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 /**
  * Created by napster on 08.03.19.
  */
@@ -44,33 +43,32 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles({"test"})
 public class RequestAuthorizationFilterTest {
 
-  @Autowired
-  protected MockMvc mvc;
+  @Autowired protected MockMvc mvc;
 
-  @Autowired
-  protected ServerConfig serverConfig;
+  @Autowired protected ServerConfig serverConfig;
 
-  @Autowired
-  protected AppInfo appInfo;
+  @Autowired protected AppInfo appInfo;
 
   @Test
   public void unauthenticatedRequest_Fail() throws Exception {
-    this.mvc.perform(get("/loadtracks"))
-      .andExpect(status().isUnauthorized());
+    this.mvc.perform(get("/loadtracks")).andExpect(status().isUnauthorized());
   }
 
   @Test
   public void wrongAuthenticatedRequest_Fail() throws Exception {
-    this.mvc.perform(get("/loadtracks")
-      .header("Authorization", serverConfig.getPassword() + "foo"))
-      .andExpect(status().isForbidden());
+    this.mvc
+        .perform(
+            get("/loadtracks")
+                .header("Authorization", serverConfig.getPassword() + "foo"))
+        .andExpect(status().isForbidden());
   }
 
   @Test
   public void authenticatedRequest_Success() throws Exception {
-    this.mvc.perform(get("/version")
-      .header("Authorization", serverConfig.getPassword()))
-      .andExpect(status().isOk())
-      .andExpect(content().string(appInfo.getVersionBuild()));
+    this.mvc
+        .perform(
+            get("/version").header("Authorization", serverConfig.getPassword()))
+        .andExpect(status().isOk())
+        .andExpect(content().string(appInfo.getVersionBuild()));
   }
 }
