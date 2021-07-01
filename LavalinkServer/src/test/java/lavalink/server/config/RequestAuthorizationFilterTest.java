168104fafd6ba33abccb9d5ed0de1a89963fa8f6
David Amorim <davidfilipeamorim@gmail.com>
2021-07-01 19:02:44 +0100

    Update tests to JUnit5

--- LavalinkServer/src/test/java/lavalink/server/config/RequestAuthorizationFilterTest.java
+++ LavalinkServer/src/test/java/lavalink/server/config/RequestAuthorizationFilterTest.java
@@ -25,13 +25,11 @@ package lavalink.server.config;
 
 
 import lavalink.server.info.AppInfo;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -41,7 +39,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * Created by napster on 08.03.19.
  */
-@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest()
 @ActiveProfiles({"test"})
